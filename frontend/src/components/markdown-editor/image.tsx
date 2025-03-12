94f57ff1d665d4390227b0a4fac43e97cf27fb08
Amit Kumar <amitkumar93525@gmail.com>
2025-03-12 12:51:13 +0530

    feat: enhance alt text functionality in image editor and update styles

--- frontend/src/components/markdown-editor/image.tsx
+++ frontend/src/components/markdown-editor/image.tsx
@@ -1,35 +1,30 @@
 import { TiptapImage, UploadImagesPlugin } from 'novel';
 import { NodeViewWrapper, NodeViewWrapperProps, ReactNodeViewRenderer } from '@tiptap/react';
 import { cx } from "class-variance-authority";
+import { useState } from 'react';
+
 
 function ImageNode(props: NodeViewWrapperProps) {
   const { updateAttributes } = props
   const { src, alt } = props.node.attrs
+  const [readOnly, setReadOnly] = useState(true)
 
   let className = 'image'
   if (props.selected) { className += ' ProseMirror-selectednode'}
 
-  const onEditAlt = () => {
-    const newAlt = prompt('Set alt text:', alt || '')
-    updateAttributes({alt: newAlt})
+  const onEditAlt = (e) => {
+    updateAttributes({alt: e.target.value})
   }
 
   return (
     <NodeViewWrapper className={className} data-drag-handle>
-      <img src={src} alt={alt} />
+      <img title={alt} src={src} alt={alt} />
       <span className="alt-text-indicator">
         { alt ?
-            <span className="symbol symbol-positive">✔</span> :
-            <span className="symbol symbol-negative">!</span>
+            <><span className="symbol symbol-positive">✅</span><span className="text">Alt text:</span></> :
+            <><span className="symbol symbol-negative">❗</span><span className="text">Alt text:</span></>
         }
-        { alt ?
-          <span className="text">Alt text: "{alt}".</span>:
-          <span className="text">Alt text missing.</span>
-        }
-
-        <button className="edit" type="button" onClick={onEditAlt}>
-          Edit
-        </button>
+        &nbsp;<input title={alt} onMouseDown={() => {setReadOnly(false)}} onBlur={() => setReadOnly(true)} readOnly={readOnly} placeholder='Enter alt text...' value={alt} onChange={onEditAlt} className='bg-transparent outline-none read-only:truncate'/>
       </span>
     </NodeViewWrapper>
   )
